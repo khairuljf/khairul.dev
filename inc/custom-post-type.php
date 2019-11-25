79148417757c7f28103a67f96157e02a48424508
khairul Islam <khairuljf@gmail.com>
2019-11-25 07:12:23 +0600

    Meta box added

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -17,6 +17,9 @@ if($activeContct){
     add_filter('manage_sunlight-contact_posts_columns','sunlight_set_contact_columns'); // manage_WillCtpName_posts_columns
     add_action('manage_sunlight-contact_posts_custom_column', 'sunlight_contact_custom_column', 10, 2);
 
+    add_action( 'add_meta_boxes', 'sunlight_contact_add_meta_box' );
+    add_action('save_post', 'sunlight_save_contact_email_data');
+
 }
 
 /*CONTACT CPT*/
@@ -58,6 +61,7 @@ function sunlight_set_contact_columns($columns){
     return $newColums;
 }
 
+// Colums data show
 function sunlight_contact_custom_column($column, $post_id){
 
     switch( $column ){
@@ -69,4 +73,48 @@ function sunlight_contact_custom_column($column, $post_id){
             echo 'email addresh';
             break;
     }
+}
+
+/*Contact meta boxes*/
+function sunlight_contact_add_meta_box(){
+    add_meta_box('contact_email', 'User Email','sunlight_contact_email_cb' , 'sunlight-contact', 'side', 'default' );
+}
+
+// Meta box callback function
+function sunlight_contact_email_cb($post){
+    wp_nonce_field('sunlight_save_contact_email_data','sunlight_contact_meta_box_nonce');
+    $value = get_post_meta($post->ID, '_contact_email_value_key', true  ); // Need must _ before key
+    echo '<label for="sunlight_contact_email_field">User Email Adress: </label>';
+    echo '<input id="sunlight_contact_email_field" name="sunlight_contact_email_field" type="email"  value="'.esc_attr($value).'" size="30" /> ';
+}
+
+// Nonce action function
+function sunlight_save_contact_email_data( $post_id ) {
+
+
+    if( ! isset($_POST['sunlight_contact_meta_box_nonce']) ){
+        return;
+    }
+
+//    if( ! wp_verify_nonce('sunlight_contact_meta_box_nonce','sunlight_save_contact_email_data') ){
+//        return;
+//    }
+
+    if( define('DOING_AUTOSAVE') && DOING_AUTOSAVE ){
+        return ;
+    }
+
+    if( ! current_user_can('edit_post', $post_id) ) {
+        return;
+    }
+
+    if( !isset( $_POST['sunlight_contact_email_field'] )){
+        return;
+    }
+
+    $my_data =  sanitize_text_field( $_POST['sunlight_contact_email_field'] );
+
+    var_dump($my_data);
+
+    update_post_meta($post_id,'_contact_email_value_key',$my_data);
 }
