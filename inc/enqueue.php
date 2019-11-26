232867ec27e9b6ee042b63a0dea085657371c7ed
khairul Islam <khairuljf@gmail.com>
2019-11-26 08:21:02 +0600

    basic function added

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -29,12 +29,29 @@ function sunlight_load_admin_script( $hook){
         return ;
     }
 
-
-
-
 }
 
 add_action( 'admin_enqueue_scripts', 'sunlight_load_admin_script' );
 
+
+/*
+	========================
+		Front-end enque function
+	========================
+*/
+
+
+function sunligt_load_scripts(){
+
+    wp_enqueue_style('sunlight_admin', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',  array(), '4.3.1', $media = 'all');
+    wp_enqueue_style('sunlight_css', get_template_directory_uri().'/css/sunlight.css',  array(), '1.0', $media = 'all');
+
+    wp_deregister_script('jquery');
+    wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.3.1.slim.min.js', false, '4.3.1',  true);
+    wp_enqueue_script('bootstrap-script', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',array('jquery'), '4.3.1',  true);
+
+}
+
+add_action('wp_enqueue_scripts', 'sunligt_load_scripts');
 
 
