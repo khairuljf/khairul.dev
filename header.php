26b05d014ce68e21b7f257aaecc966c844b37865
khairul Islam <khairuljf@gmail.com>
2019-11-26 21:27:34 +0600

    New font added

--- header.php
+++ header.php
@@ -16,6 +16,26 @@
 
 <body <?php body_class(); ?> >
 
+<div class="container-fluid p-0">
+
+    <div class="row">
+
+        <div class="col-12">
+            <div class="header-container text-center background-image d-flex align-items-center justify-content-center" style="background-image: url('<?php header_image(); ?>')">
+                 <div class="header-content">
+                     <h1 class="site-title">
+                       <span class="d-none"><?php bloginfo('name'); ?>  </span>
+                         <spna class="icon-books"></spna>
+                     </h1>
+
+                     <h2 class="blog-descripion"><?php bloginfo('description'); ?></h2>
+                 </div> <!-- .header-content -->
+                 <div class="nav-container"></div> <!-- .nav-container -->
+            </div>
+        </div> <!--.col-12 -->
+    </div> <!-- .row -->
+
+</div><!-- .container-fluid -->
 
 
 
