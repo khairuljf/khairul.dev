4afd76e8f9a6386da8991c5b7d4a230dde4995f8
khairul Islam <khairuljf@gmail.com>
2019-11-30 01:49:36 +0600

    Posted time & category printed

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -53,18 +53,24 @@
         $posted_on = human_time_diff(get_the_time('U'), current_time('timestamp'));
         $categories = get_the_category();
 
-        $separator =  ',';
+
+        $separator =  ', ';
         $output = '';
 
-        if(!$categories):
+        if($categories):
 
             foreach ($categories as $category) {
-                $output .= '<a href="'.esc_url( get_the_category_link($category->term_id) ).'" alt="'.esc_attr('View All  Posts in %s', $category->name).'">'.esc_html( 'View All  Posts in %s', $category->name).'</a>';
+
+                if($output){
+                    $output .=$separator;
+                }
+
+                $output .= '<a href="'.esc_url( get_category_link($category->term_id) ).'" alt="'.esc_attr('View All  Posts in %s', $category->name).'">'.esc_html($category->name).'</a>';
             }
             endif;
 
-        return '<span class="posted-on">  Posted <a href="'.esc_url( get_permalink() ).'"> '.$posted_on.' </a>  </span>
-                <span class="posted-in"> '. get_the_category(',') .' </span>';
+        return '<span class="posted-on">  Posted <a href="'.esc_url( get_permalink() ).'"> '.$posted_on.' </a>  ago</span>
+                <span class="posted-in"> '. $output .' </span>';
     }
 
     function sunlight_posted_footer(){
