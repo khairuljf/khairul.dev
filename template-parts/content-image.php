6edbcf316e22b27e3ac1592e97c42b5112aa5543
khairul Islam <khairuljf@gmail.com>
2019-11-30 15:41:10 +0600

    Post format image content added with style

--- template-parts/content-image.php
+++ template-parts/content-image.php
@@ -14,16 +14,18 @@
 
 
     <?php if(has_post_thumbnail()): ?>
-    <header class="entry-header text-center" style="background-image: url("<?php the_post_thumbnail_url() ?>")">
+    <header class="entry-header text-center" style="background-image: url(<?php echo sunlight_get_attachment() ?>);height: 500px;" >
         <?php the_title('<h1 class="entry-title">','</h1>'); ?>
         <div class="entry-meta">
             <?php echo sunlight_posted_meta(); ?>
         </div>
+
+        <div class="entry-excerpt">
+            <?php the_excerpt(); ?>
+        </div>
+
     </header>
     <?php endif; ?>
-
-
-
 
     <div class="entery-footer">
         <?php echo sunlight_posted_footer(); ?>
